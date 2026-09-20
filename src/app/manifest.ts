31c2212d94a437833a7e685caba7acc241b6b566
Destiny Tch <182261473+DestinyTch@users.noreply.github.com>
2026-09-20 01:47:02 +0100

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "SafeCrib",
@@ -11,8 +13,16 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#FFFFFF",
     theme_color: "#FFFFFF",
     icons: [
-      { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-      { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
+      {
+        src: "/icons/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icons/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
     ],
   };
-}
+} 

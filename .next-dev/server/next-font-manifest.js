46907ed56472ce72e3c7a245b1ff6a37d46085c0
skul_Africa <skultechfficial@gmail.com>
2026-09-24 17:13:56 +0000

    updaed

--- .next-dev/server/next-font-manifest.js
+++ .next-dev/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/workspaces/crib/src/app/layout\":[\"static/media/13971731025ec697-s.p.woff2\",\"static/media/4c9affa5bc8f420e-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
